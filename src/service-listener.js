98410405be0441dbee54845713c4745cb565c6fb
Pierre Meunier <pierre.meunier@wealthwizards.com>
2018-10-30 16:34:42 +0000

    fix: ignore deployment updates while a ReplicaSet is being updated

--- src/service-listener.js
+++ src/service-listener.js
@@ -9,8 +9,31 @@ const { updateConsul } = require('./deployment-change-handlers/update-consul');
 const { updateServiceRegistry } = require('./deployment-change-handlers/update-service-registry');
 const { updateVersionService } = require('./deployment-change-handlers/update-version-service');
 
+function isUpdateAboutOldDeployment(deployment) {
+  return deployment.object.metadata.generation !== deployment.object.status.observedGeneration;
+}
+
+function isStatusAboutOngoingDeployment(deployment) {
+  return !deployment.object.status.conditions.some(c => c.reason === 'NewReplicaSetAvailable');
+}
+
+function ignoreUpdateAboutOngoingDeployment(deployment) {
+  if (deployment.type !== 'MODIFIED') {
+    return false;
+  }
+
+  return isUpdateAboutOldDeployment(deployment) || isStatusAboutOngoingDeployment(deployment);
+}
+
 function onDeploymentChange(deploymentObject) {
   logger.trace('Received deployment object: ', JSON.stringify(deploymentObject, null, 2));
+
+  if (ignoreUpdateAboutOngoingDeployment(deploymentObject)) {
+    // Ignoring all updates about ongoing deployment
+    // We only want to send update once a deployment is done
+    return;
+  }
+
   updateConsul(deploymentObject);
   updateServiceRegistry(deploymentObject);
   updateVersionService(deploymentObject);
