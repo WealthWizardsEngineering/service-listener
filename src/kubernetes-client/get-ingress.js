474e619a088dd3979c7926ab08b504fb55580eb2
dave-shepherd <david.shepherd@wealthwizards.com>
2022-02-04 08:36:02 +0000

    Update ingress api definition

--- src/kubernetes-client/get-ingress.js
+++ src/kubernetes-client/get-ingress.js
@@ -3,7 +3,7 @@ const k8s = require('@kubernetes/client-node');
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 
-const k8sApi = kc.makeApiClient(k8s.ExtensionsV1beta1Api);
+const k8sApi = kc.makeApiClient(k8s.NetworkingV1Api);
 
 async function getIngress(namespace, ingressName) {
   return k8sApi.readNamespacedIngress(ingressName, namespace);
